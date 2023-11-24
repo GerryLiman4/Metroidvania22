997602f318fc9fe4aa392c64cbe9bcd444a78dbb
Route126 <150384215+Route126@users.noreply.github.com>
2023-11-23 20:26:24 -0600

    Working on Dialogue & Simple Enemy AI
    Working on Dialogue & Simple Enemy AI

---
+++ Dialogue/Helpers/actionable.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+# Custom Dialgoue Balloon with portriats
+const Balloon = preload("res://Dialogue/Dialogue/portrait_balloon.tscn")
+
+# Dialogue file and start positions
+@export var dialogue_resourse : DialogueResource
+@export var dialogue_start : String = "start"
+
+func action() -> void:
+	var balloon : Node = Balloon.instantiate()
+	get_tree().current_scene.add_child(balloon)
+	#balloon.start(dialogue_resourse, dialogue_start)
+	#DialogueManager.show_dialogue_balloon(dialogue_resourse, dialogue_start)
+	DialogueManager.show_example_dialogue_balloon(dialogue_resourse, dialogue_start)
