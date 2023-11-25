27aa163f69a39060ad91e44ecf1f8494fe4b01ca
Route126 <150384215+Route126@users.noreply.github.com>
2023-11-25 12:32:47 -0600

    Added main menu & intro cutscene
    Added main menu & intro cutscene

---
+++ Script/Singleton/AudioController.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@onready var music_player = $AudioStreamPlayer
+
+@onready var main_menu_loop : AudioStreamOggVorbis = preload("res://UI/cowboy.ogg")
+
+
+func _ready():
+	#music_player.stream = main_menu_loop
+	music_player.playing = true
+	
+func handle_music_change():
+	music_player.stop()
+	match SceneTransition.curr_scene:
+		SceneTransition.scenes.Menu:
+			music_player.stream = main_menu_loop
+		SceneTransition.scenes.Game:
+			music_player.stream = null
+		SceneTransition.scenes.Cutscene:
+			music_player.stream = main_menu_loop
+			
+	music_player.playing = true
---
+++ Script/Singleton/SceneTransition.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+var curr_scene = scenes.Menu
+
+enum scenes {
+	Menu,
+	Cutscene,
+	Game,
+}
+
+var new_scene : String
+
+@onready var animation_player = $AnimationPlayer
+
+
+func start_transition_to(scene_name : String, change_music : bool, path_to_scene : String) -> void:
+	match scene_name:
+		"menu":
+			curr_scene = scenes.Menu
+		"game":
+			curr_scene = scenes.Game
+		"cutscene":
+			curr_scene = scenes.Cutscene
+	
+	if change_music == true:
+		AudioController.handle_music_change()
+	
+	new_scene = path_to_scene
+	animation_player.play("change_scene_to_file")
+	
+func change_scene_to_file() -> void:
+	# Make sure path is correct with assert
+	var __ = get_tree().change_scene_to_file(new_scene) == OK
+	assert(__)
